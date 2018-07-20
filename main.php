d2edc51c94c89b6a0b3c0d971ec7d0e291e86eac
novgorodskii <novgorodskiji@gmail.com>
2018-07-20 15:41:38 +0300

    commit add form action

--- main.php
+++ main.php
@@ -18,7 +18,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('zvezdacarmail@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('novgorodskiji@gmail.com');     // Кому будет уходить письмо 
+$mail->addAddress('viktorsi@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
